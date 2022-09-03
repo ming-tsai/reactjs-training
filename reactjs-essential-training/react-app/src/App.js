32b0b0718b0e4c4b573726fdb89e7fda71c46605
Ming Tsai <ming.jia1213@gmail.com>
2022-09-03 11:23:30 -0400

    chore(essentail-training): finished chapter 04-06
    Understanding the dependency array on useEffect

--- reactjs-essential-training/react-app/src/App.js
+++ reactjs-essential-training/react-app/src/App.js
@@ -3,10 +3,15 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
 
   useEffect(() => {
-    console.log(`It's ${emotion} right now.`)
-  }, [emotion])
+    console.log(`It's ${emotion} around here!`)
+  }, [emotion, secondary])
+
+  useEffect(() => {
+    console.log(`It's ${secondary} around here!`)
+  }, [secondary])
   return (
     <div className="App">
       <h1>Current emotion is {emotion}</h1>
@@ -15,6 +20,10 @@ function App() {
       </button>
       <button onClick={() => setEmotion("excited")}>
         Excited
+      </button>
+      <h1>Current secondary emotion is {secondary}</h1>
+      <button onClick={() => setSecondary("greatful")}>
+        Greatful
       </button>
     </div>
   );
