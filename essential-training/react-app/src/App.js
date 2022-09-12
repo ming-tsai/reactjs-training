05e00800882a1b16acccf772e2f9a3d670b78d5c
Ming Tsai <ming.jia1213@gmail.com>
2022-09-11 21:42:51 -0400

    chore(essential-training): finished chapter 06
    Render using props

--- essential-training/react-app/src/App.js
+++ essential-training/react-app/src/App.js
@@ -1,49 +1,35 @@
 import './App.css';
-import { useState, useEffect } from "react";
 
-function GithubUser({name, location, avatar}) {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <p>{location}</p>
-      <img src={avatar} height={150} alt={name} />
-    </div>
-  )
+const tahoe_peaks = [
+  {name: "Freel", elevation: 1091},
+  {name: "Monument", elevation: 10067},
+  {name: "Pyramid", elevation: 9983},
+  {name: "Tallac", elevation: 9735},
+];
+
+function List({data, renderItem, renderEmpty}) {
+  return !data.length ?
+    (renderEmpty) :
+    (<ul>
+      {data.map((item) => (
+        <li key={item.name} >
+          {renderItem(item)}
+        </li>
+      ))}
+    </ul>)
 }
 
 function App() {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    fetch('https://api.github.com/users/ming-tsai')
-      .then((response) => response.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError)
-  }, []);
-  if (loading) {
-    return (<h1>loading...</h1>)
-  }
-
-  if (error) {
-    return (<pre>{JSON.stringify(error)}</pre>)
-  }
-
-  if(!data) {
-    return null;
-  } 
-  if (data) {
-    return (
-      <GithubUser
-        location={data.location}
-        name={data.name}
-        avatar={data.avatar_url} />
-    );
-  }
-  return <h1>Data</h1>
+  return (
+    <List
+      data={tahoe_peaks}
+      renderEmpty={<p>The list is empty.</p>}
+      renderItem={(item) =>
+        <>
+          {item.name} - {item.elevation} ft.
+        </>
+      } />
+  )
 }
 
 export default App;
