fe9b3db4fefef2f9ee103d61151f6f6c75989710
Ming Tsai <ming.jia1213@gmail.com>
2022-09-11 21:49:09 -0400

    chore(essential-training): finished chapter 07-01
    Add react router library

--- essential-training/react-app/src/App.js
+++ essential-training/react-app/src/App.js
@@ -1,35 +1,19 @@
 import './App.css';
 
-const tahoe_peaks = [
-  {name: "Freel", elevation: 1091},
-  {name: "Monument", elevation: 10067},
-  {name: "Pyramid", elevation: 9983},
-  {name: "Tallac", elevation: 9735},
-];
+function Home() {
+  return <h1>My website.</h1>
+}
 
-function List({data, renderItem, renderEmpty}) {
-  return !data.length ?
-    (renderEmpty) :
-    (<ul>
-      {data.map((item) => (
-        <li key={item.name} >
-          {renderItem(item)}
-        </li>
-      ))}
-    </ul>)
+function About() {
+  return <h1>About us.</h1>
+}
+
+function Contact() {
+  return <h1>Contact us.</h1>
 }
 
 function App() {
-  return (
-    <List
-      data={tahoe_peaks}
-      renderEmpty={<p>The list is empty.</p>}
-      renderItem={(item) =>
-        <>
-          {item.name} - {item.elevation} ft.
-        </>
-      } />
-  )
+  return <Home />
 }
 
 export default App;
