a3e1a1a5475532bd1c4ec11d2956cd2bbd3b583c
Ming Tsai <ming.jia1213@gmail.com>
2022-09-14 21:01:55 -0400

    chore(essential-training): finished chapter 07-02
    Configured router for React

--- essential-training/react-app/src/App.js
+++ essential-training/react-app/src/App.js
@@ -1,19 +1,17 @@
 import './App.css';
 
-function Home() {
+export function Home() {
   return <h1>My website.</h1>
 }
 
-function About() {
+export function About() {
   return <h1>About us.</h1>
 }
 
-function Contact() {
+export function Contact() {
   return <h1>Contact us.</h1>
 }
 
-function App() {
+export function App() {
   return <Home />
 }
-
-export default App;
